e394b9d17087a4abe977c2019c927e36d659b454
ssack <ssack@student.ethz.ch>
2019-01-31 15:29:32 +0100

    Updated to actual simulation parameters

--- dotCavity/dot_cav_full_info.py
+++ dotCavity/dot_cav_full_info.py
@@ -7,46 +7,48 @@ import numpy as np
 def dmrg(epsImp, epsCav):
 
     # define the simulation parameters
-    D = 4
+    D = 40
     d = 4
     Lambda = 2.0
-    length = 3
+    length = 20
     U = 0.5
+    omega = 0.025
+    tL, tR = 0.05, 0.05
 
     # setting up the Hamiltonian in MPO from
     c = bathCouplings(density='const', N=length, Lambda=Lambda)
-    mpo = dotCavity(tL=0.05, tR=0.05, tBathLeft=c.t, tBathRight=c.t, omega=0.025, epsDot=epsImp, epsCav=epsCav,
+    mpo = dotCavity(tL=tL, tR=tR, tBathLeft=c.t, tBathRight=c.t, omega=omega, epsDot=epsImp, epsCav=epsCav,
               epsBathLeft=c.eps, epsBathRight=c.eps, U=U)
-    H = MPS.MPO(numberOfSites=len(mpo.Hamiltonian), bondDimension=6, localHilbertSpace=4, maximalBondDimension=D,
+    H = MPS.MPO(numberOfSites=len(mpo.Hamiltonian), bondDimension=6, localHilbertSpace=d, maximalBondDimension=D,
               thresholdEntanglement=0., periodic=False)
     H.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.Hamiltonian])
     H.structuredPhysicalLegs = True
 
     groundState = MPS.mpsDMRG(H, bondDimension=D, thresholdEntanglement=1e-6)
-    groundState.groundState(sweeps=1)
+    groundState.groundState(sweeps=5)
 
     length_new = len(groundState.M)
 
-    dot_up =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
+    dot_up =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=d, maximalBondDimension=D,
                     thresholdEntanglement=0., periodic=False)
     dot_up.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_dot_up])
     dot_up.d = [d] * length_new
     dot_up.structuredPhysicalLegs = True
 
 
-    dot_down =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
+    dot_down =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=d, maximalBondDimension=D,
                       thresholdEntanglement=0., periodic=False)
     dot_down.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_dot_down])
     dot_down.d = [d] * length_new
     dot_down.structuredPhysicalLegs = True
 
-    cav_up =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
+    cav_up =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=d, maximalBondDimension=D,
                     thresholdEntanglement=0., periodic=False)
     cav_up.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_cav_up])
     cav_up.d = [d] * length_new
     cav_up.structuredPhysicalLegs = True
 
-    cav_down =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
+    cav_down =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=d, maximalBondDimension=D,
                       thresholdEntanglement=0., periodic=False)
     cav_down.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_cav_down])
     cav_down.d = [d] * length_new
@@ -88,13 +90,10 @@ def dmrg(epsImp, epsCav):
     dummySize = dens.shape[0] * dens.shape[1]
     dens = np.reshape(dens,(dummySize, dummySize))
 
-
+    # Calculate the observables
     purity = np.real(np.trace(dens @ dens))
-
-
     dot_occ = np.real(groundState.conjugate() * (dot_up + dot_down) * groundState)
     cav_occ = np.real(groundState.conjugate() * (cav_up + cav_down) * groundState)
-
     total_occ = dot_occ + cav_occ
 
     return correlation, purity, total_occ, dot_occ, cav_occ
