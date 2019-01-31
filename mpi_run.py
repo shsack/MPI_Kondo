4208eaa5c6fffa3dd91db14a18583b7ea5e61405
ssack <ssack@student.ethz.ch>
2019-01-31 17:57:00 +0100

    Changes to data structure and data distribution.

--- mpi_run.py
+++ mpi_run.py
@@ -1,37 +1,44 @@
 from mpi4py import MPI
-# from multiprocessing import Pool, cpu_count
 from itertools import product
 from dotCavity import dmrg as main
 import numpy as np
 # from joblib import Parallel, delayed
-
-def split(rank, size, *data):
-
-    """Split the data into chunks"""
-
-    partitions = [int(len(d) / size) for d in data]
-    return [d[rank * partition:(rank + 1) * partition] for partition, d in zip(partitions, data)]
+# import multiprocessing as mp
 
 
-def parallel_apply(data):
+def split_data(size, data):
+
+    n = len(data) // size
+    return [data[i:i + n] for i in range(0, len(data), n)]
+
+
+
+def apply_main_in_node(data):
 
     """Feed parameters into function."""
 
-    #TODO: put for loop!
-
     # p = Pool(processes=1)
-    # result = p.starmap(dmrg, product(*data))
+    # result = p.starmap(main, data)
+    # return result
     # p.close()  # shut down the pool
     # p.join()
 
+    # result = Parallel(n_jobs=4)(delayed(dmrg)(*i) for i in product(*data))
+    # return result
+
+    # jobs = []
+    # for d in product(*data):
+    #     p = mp.Process(target=main, args=(*d,))
+    #     jobs.append(p)
+    #     p.start()
+    #
+    # return jobs
+
     result = []
-    for d in product(*data):
+    for d in data:
         result.append(main(*d))
 
     return result
-
-    # result = Parallel(n_jobs=4)(delayed(dmrg)(*i) for i in product(*data))
-    # return result
 
 
 # MPI setup
@@ -41,18 +48,28 @@ size = comm.Get_size() # Number of nodes
 
 
 # Define simulation parameters
-epsImp = np.linspace(-1./16, 0.5/16, 50)
-epsCav = np.linspace(-0.5/16, 0.5/16, 50)
+num_data_points = 4 # !!! Has to be a multiple of the requested nodes !!! <---- IMPORTANT
+epsImp = np.linspace(-1./16, 0.5/16, num_data_points)
+epsCav = np.linspace(-0.5/16, 0.5/16, num_data_points)
+data = list(product(epsImp, epsCav))
 
 
-# Split the data for the nodes and apply the function to every node
-data = split(rank, size, epsImp, epsCav)
-result = parallel_apply(data)
+# Split the data in the zeroth node
+if rank == 0:
 
+    data_split = split_data(size, data)
+
+else :
+
+    data_split = None
+
+
+# Scatter data from zeroth node onto other nodes and do the calculation in each node
+data_in_node = comm.scatter(data_split, root=0)
+result = apply_main_in_node(data_in_node)
 
 # Combine the results from the nodes
 result = comm.gather(result, root=0)
-data = comm.gather(data, root=0)
 
 # Save the results in a text file
 if rank == 0:
@@ -64,3 +81,4 @@ if rank == 0:
             f.write(' '.join(map(str, dat_i)) + " " + ' '.join(map(str, res_i)) + '\n')
 
     f.close()
+
