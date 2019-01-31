f78cc855360b9cb8e5d909c848f2fc0a6624f1dc
ssack <ssack@student.ethz.ch>
2019-01-31 15:30:00 +0100

    Added a plot script, to be used with caution! Axes might be swapped

---
+++ plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+# Plotting style
+plt.style.use('bmh')
+
+# File to import
+file = 'dot_occupation_purity_high_bond.txt'
+
+data = pd.read_csv(file, delimiter=' ', header=None, dtype=float)
+epsImp = list(data[data.columns[0]])
+epsCav = list(data[data.columns[1]])
+
+# Choose the observable as a column of the txt file
+num_obs = 3
+observable = list(data[data.columns[num_obs]])
+
+data = data.pivot(index=0, columns=1, values=2).T
+
+extent = [min(epsImp), max(epsImp), min(epsCav), max(epsCav)]
+plt.imshow(data, origin='lower', aspect=1.5, extent=extent)
+# plt.imshow(data, origin='lower', aspect=1./1.5, extent=extent, vmin=0, vmax=2)
+
+# Label depending of which observable is plotted
+cbar = plt.colorbar()
+cbar.set_label(r'$\langle n_{d\uparrow} + n_{d\downarrow} \rangle$')
+plt.xlabel(r'$\epsilon_d$')
+plt.ylabel(r'$\epsilon_c$')
+fig = plt.gcf()
+fig.savefig('heatmap.pdf')
