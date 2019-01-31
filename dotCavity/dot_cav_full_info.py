1362f2326437ff8b3315869de657a8aba342547f
ssack <ssack@student.ethz.ch>
2019-01-31 14:23:10 +0100

    Uncommented rest of calculation

--- dotCavity/dot_cav_full_info.py
+++ dotCavity/dot_cav_full_info.py
@@ -27,51 +27,51 @@ def dmrg(epsImp, epsCav):
 
     length_new = len(groundState.M)
 
-    # dot_up =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
-    #                 thresholdEntanglement=0., periodic=False)
-    # dot_up.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_dot_up])
-    # dot_up.d = [d] * length_new
-    # dot_up.structuredPhysicalLegs = True
-    #
-    #
-    # dot_down =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
-    #                   thresholdEntanglement=0., periodic=False)
-    # dot_down.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_dot_down])
-    # dot_down.d = [d] * length_new
-    # dot_down.structuredPhysicalLegs = True
-    #
-    # cav_up =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
-    #                 thresholdEntanglement=0., periodic=False)
-    # cav_up.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_cav_up])
-    # cav_up.d = [d] * length_new
-    # cav_up.structuredPhysicalLegs = True
-    #
-    # cav_down =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
-    #                   thresholdEntanglement=0., periodic=False)
-    # cav_down.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_cav_down])
-    # cav_down.d = [d] * length_new
-    # cav_down.structuredPhysicalLegs = True
-    #
-    # cov_dot_up_cav_down = np.real(groundState.conjugate() * (dot_up * cav_down) * groundState - \
-    #        (groundState.conjugate() * dot_up * groundState) * (groundState.conjugate() * cav_down * groundState))
-    #
-    # std_dot_up = np.sqrt(np.real(groundState.conjugate() * (dot_up * dot_up) * groundState -
-    #                 (groundState.conjugate() * dot_up * groundState)**2))
-    #
-    # std_cav_down = np.sqrt(np.real(groundState.conjugate() * (cav_down * cav_down) * groundState -
-    #                 (groundState.conjugate() * cav_down * groundState)**2))
-    #
-    #
-    # cov_dot_down_cav_up =  np.real(groundState.conjugate() * (dot_down * cav_up) * groundState - \
-    #        (groundState.conjugate() * dot_down * groundState) * (groundState.conjugate() * cav_up * groundState))
-    #
-    # std_dot_down = np.sqrt(np.real(groundState.conjugate() * (dot_down * dot_down) * groundState -
-    #                 (groundState.conjugate() * dot_down * groundState)**2))
-    #
-    # std_cav_up = np.sqrt(np.real(groundState.conjugate() * (cav_up * cav_up) * groundState -
-    #                 (groundState.conjugate() * cav_up * groundState)**2))
-    #
-    # correlation = cov_dot_up_cav_down / (std_dot_up * std_cav_down) + cov_dot_down_cav_up / (std_dot_down * std_cav_up)
+    dot_up =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
+                    thresholdEntanglement=0., periodic=False)
+    dot_up.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_dot_up])
+    dot_up.d = [d] * length_new
+    dot_up.structuredPhysicalLegs = True
+
+
+    dot_down =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
+                      thresholdEntanglement=0., periodic=False)
+    dot_down.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_dot_down])
+    dot_down.d = [d] * length_new
+    dot_down.structuredPhysicalLegs = True
+
+    cav_up =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
+                    thresholdEntanglement=0., periodic=False)
+    cav_up.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_cav_up])
+    cav_up.d = [d] * length_new
+    cav_up.structuredPhysicalLegs = True
+
+    cav_down =  MPS.MPO(numberOfSites=length_new, bondDimension=1, localHilbertSpace=4, maximalBondDimension=D,
+                      thresholdEntanglement=0., periodic=False)
+    cav_down.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in mpo.n_cav_down])
+    cav_down.d = [d] * length_new
+    cav_down.structuredPhysicalLegs = True
+
+    cov_dot_up_cav_down = np.real(groundState.conjugate() * (dot_up * cav_down) * groundState - \
+           (groundState.conjugate() * dot_up * groundState) * (groundState.conjugate() * cav_down * groundState))
+
+    std_dot_up = np.sqrt(np.real(groundState.conjugate() * (dot_up * dot_up) * groundState -
+                    (groundState.conjugate() * dot_up * groundState)**2))
+
+    std_cav_down = np.sqrt(np.real(groundState.conjugate() * (cav_down * cav_down) * groundState -
+                    (groundState.conjugate() * cav_down * groundState)**2))
+
+
+    cov_dot_down_cav_up =  np.real(groundState.conjugate() * (dot_down * cav_up) * groundState - \
+           (groundState.conjugate() * dot_down * groundState) * (groundState.conjugate() * cav_up * groundState))
+
+    std_dot_down = np.sqrt(np.real(groundState.conjugate() * (dot_down * dot_down) * groundState -
+                    (groundState.conjugate() * dot_down * groundState)**2))
+
+    std_cav_up = np.sqrt(np.real(groundState.conjugate() * (cav_up * cav_up) * groundState -
+                    (groundState.conjugate() * cav_up * groundState)**2))
+
+    correlation = cov_dot_up_cav_down / (std_dot_up * std_cav_down) + cov_dot_down_cav_up / (std_dot_down * std_cav_up)
 
 
     groundState.makeCanonical('Right')
@@ -92,15 +92,10 @@ def dmrg(epsImp, epsCav):
     purity = np.real(np.trace(dens @ dens))
 
 
-    # dot_occ = np.real(groundState.conjugate() * (dot_up + dot_down) * groundState)
-    # cav_occ = np.real(groundState.conjugate() * (cav_up + cav_down) * groundState)
+    dot_occ = np.real(groundState.conjugate() * (dot_up + dot_down) * groundState)
+    cav_occ = np.real(groundState.conjugate() * (cav_up + cav_down) * groundState)
 
-    # total_occ = dot_occ + cav_occ
+    total_occ = dot_occ + cav_occ
 
-    # return correlation, purity, total_occ, dot_occ, cav_occ
+    return correlation, purity, total_occ, dot_occ, cav_occ
 
-    return purity, 0
-
-def test(x, y):
-
-    return x + y, x * y
