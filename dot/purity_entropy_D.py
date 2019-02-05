Fixed purity entropy calculation.

dot/purity_entropy_D.py
@@ -13,19 +13,13 @@ def setup(epsImp, U, V, Lambda,length, D, sweeps):
     thresholdEntanglement=0., periodic=False)
     anderson.M = copy.deepcopy([np.transpose(h, (0, 2, 3, 1)) for h in dot_.Hamiltonian])
     anderson.structuredPhysicalLegs = True
-    groundState = MPSModule.mpsDMRG(anderson, bondDimension=D, thresholdEntanglement=1-6)
+    groundState = MPSModule.mpsDMRG(anderson, bondDimension=D, thresholdEntanglement=1e-6)
     groundState.groundState(sweeps=sweeps)
 
     return groundState
 
 
-def main(V, D):
-
-    U = 0.5
-    epsImp = U/2
-    Lambda = 2.0
-    length = 20
-    sweeps = 10
+def main(D, V, epsImp, U, Lambda, length, sweeps):
 
     groundState = setup(epsImp=epsImp, U=U, V=V, Lambda=Lambda, length=length, D=D, sweeps=sweeps)
     groundState.makeCanonical('Right')