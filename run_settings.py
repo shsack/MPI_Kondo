9492e5ad83bab21cfa1bc8d842176a522504f2da
ssack <ssack@student.ethz.ch>
2019-02-05 16:14:18 +0100

    Updated to Piz Daint settings.

--- run_settings.py
+++ run_settings.py
@@ -10,11 +10,11 @@ def import_main_data(which_simulation):
         from dot_cavity.dot_cav_full_info import main
 
         # Define the simulation parameters
-        num_data_points = 16  # !!! Has to be a multiple of the requested nodes !!! <---- IMPORTANT
-        D = 10
-        sweeps = 10
+        num_data_points = 72  # !!! Has to be a multiple of the requested nodes !!! <---- IMPORTANT
+        D = 40
+        sweeps = 5
         Lambda = 2.0
-        length = 10  # The length of the whole chain is twice this value
+        length = 20  # The length of the whole chain is twice this value
         U = 0.5
         omega = 0.1
         tL, tR = 0.01, 0.01
@@ -31,35 +31,37 @@ def import_main_data(which_simulation):
 
         from dot.purity_entropy_D import main
 
+        # Define the simulation parameters
         V = 0.1
         U = 0.5
         epsImp = -U / 2
         Lambda = 2.0
         length = 20
-        sweeps = 10
-        D = range(10, 70)
-        main = partial(main, V=V, U=U, epsImp=epsImp, Lambda=Lambda, lenght=length, sweeps=sweeps)
+        sweeps = 5
+        D = list(range(10, 71))
 
-        # TODO: DOES NOT YET WORK
+        # Fix parameters
+        data = [(d, ) for d in D]
+        main = partial(main, V=V, U=U, epsImp=epsImp, Lambda=Lambda, length=length, sweeps=sweeps)
 
-        return D, main
+        return data, main
 
     if which_simulation == 'purity_V':
 
         from dot.purity_V import main
 
+        # Define the simulation parameters
         U = 0.5
         epsImp = -U / 2
         Lambda = 2.0
-        length = 20
-        sweeps = 10
+        length = 10
+        sweeps = 5
         D = [20, 50, 80, 110, 140, 170, 200]
-        V = np.linspace(start=0.0, stop=0.5, num=50)
+        V = np.linspace(start=0.0, stop=0.5, num=30)
 
+        # Fix parameters
         main = partial(main, U=U, epsImp=epsImp, Lambda=Lambda, length=length, sweeps=sweeps)
-        data = product(D, V)
-
-        # TODO: DOES NOT YET WORK
+        data = list(product(D, V))
 
         return data, main
 
