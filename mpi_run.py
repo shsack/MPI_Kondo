58eda4e737f1b499e26e46be43e97b5009de2c35
ssack <ssack@student.ethz.ch>
2019-02-05 11:33:22 +0100

    Updated result writing to file to work with all simulations.

--- mpi_run.py
+++ mpi_run.py
@@ -3,7 +3,7 @@ import multiprocessing as mp
 from run_settings import import_main_data
 
 # Choose the simulation you want to run
-simulation_list = ['dot_cav_purity_heatmap', 'purity_entropy_D', 'purity_V']
+simulation_list = ['dot_cav_heatmap', 'purity_entropy_D', 'purity_V']
 simulation_name = simulation_list[0]
 data, main = import_main_data(which_simulation=simulation_name)
 
@@ -16,9 +16,9 @@ def split_data(size, data):
 
 def apply_main_in_node(data):
 
-    # return [main(*d) for d in data]
-    p = mp.Pool(mp.cpu_count())
-    return p.starmap(main, data)
+    return [main(*d) for d in data]
+    # p = mp.Pool(mp.cpu_count()) # TODO: fix number of processes
+    # return p.starmap(main, data) # FIXME: works only for specific numpy BLAS library
 
 
 # MPI setup
@@ -49,8 +49,12 @@ if rank == 0:
     for res in result:
         for res_i in res:
             myRes.append(res_i)
-    for dat_, res_ in zip(data, myRes):
-        f.write(' '.join(map(str, dat_)) + " " + ' '.join(map(str, res_)) + '\n')
+    if simulation_name == simulation_list[0]:
+        for dat_, res_ in zip(data, myRes):
+            f.write(' '.join(map(str, dat_)) + " " + ' '.join(map(str, res_)) + '\n')
+    else:
+        for dat_, res_ in zip(data, myRes):
+            f.write(str(dat_) + " " + str(res_) + '\n')
 
     f.close()
 
